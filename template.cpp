54ae4d22b3de183a6726f5f215c74071fe8fbd5a
Tuan Anh Dao <kitsudo1412@gmail.com>
2022-08-08 15:22:23 +0200

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -10,6 +10,13 @@ signed main() {
   std::cin >> T;
 
   for(int t = 1; t <= T; t++) {
+    int n, l;
+    std::cin >> n >> l;
+    
+    int Array[100];
+    for(int i=0; i<100; i++)
+      cin>>Array[i];
+    
     std::cout << "Case #" << t << ": " << std::endl;
   }
 
