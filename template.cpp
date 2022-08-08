31f75e5a13a8a37997d3a857c80775a23a3e89d0
Tuan Anh Dao <kitsudo1412@gmail.com>
2022-08-08 15:04:29 +0200

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define int long long
+
+signed main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+
+  int T;
+  std::cin >> T;
+
+  for(int t = 1; t <= T; t++) {
+    std::cout << "Case #" << t << ": " << std::endl;
+  }
+
+  return 0;
+}
