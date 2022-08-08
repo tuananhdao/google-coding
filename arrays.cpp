c288be2bc90d061ca26f20505dae7c44dc5de1a7
Tuan Anh Dao <kitsudo1412@gmail.com>
2022-08-08 15:16:16 +0200

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -1,1 +1,19 @@
 std::pair<int, int> arr[n];
+
+
+// map
+#include <map>
+std::map<char, char> my_map = {
+    { 'A', '1' },
+    { 'B', '2' },
+    { 'C', '3' }
+};
+my_map.insert({ 5, 50 });
+for (auto itr = my_map.begin(); itr != my_map.end(); ++itr) {
+    cout << itr->first << '\t' << itr->second << '\n';
+}
+for (const auto& [key, value] : my_map) {
+    std::cout << '[' << key << "] = " << value << "; ";
+}
+std::cout << "my_map[A] = " << my_map["A"] << '\n';
+
